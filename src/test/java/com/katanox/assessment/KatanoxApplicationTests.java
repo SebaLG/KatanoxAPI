5a2fb33794de859c2fdbb880c304f7f2ca05c2ed
SebaLG <sebalg180@gmail.com>
2020-09-30 20:00:28 +0200

    First commit

---
+++ src/test/java/com/katanox/assessment/KatanoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katanox.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatanoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
